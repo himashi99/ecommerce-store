0fce28dad9a86ffc98f9ad1502dd833dda4c3751
Himashi Hettege Dona <himashi99@hotmail.com>
2019-07-31 21:48:23 -0400

    Added prodcuts and structure to Product page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Nav extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
         </Link>
@@ -25,28 +26,15 @@ export default class Nav extends Component {
             My Cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
 
-// Styled component
-const ButtonContainer = styled.button`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  border: 0.1rem solid var(--lightBlue);
-  color: var(--lightBlue);
-  border-radius: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  cursor: pointer;
-  margin: 0.2rem 0.5rem 0.2rem; 0.5rem;
-  transition: all 0.5s ease-in-out;
-  &:hover {
-    background: var(--lightBlue);
-    color: var(--mainBlue);
-  }
-  &:focus {
-    outline: none;
+const NavWrapper = styled.nav`
+  background var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
   }
 `;
