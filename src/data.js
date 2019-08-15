6d18f1331f9f5994971a93ddd1a14f71d2fa66a9
Himashi Hettege Dona <himashi99@hotmail.com>
2019-08-15 08:04:30 -0400

    Updated default product in data list

--- src/data.js
+++ src/data.js
@@ -104,10 +104,10 @@ export const storeProducts = [
 
 export const detailProduct = {
   id: 1,
-  title: "Google Pixel - Black",
+  title: "La Petite Tie-Back Babydoll Dress",
   img: "img/product-1.png",
-  price: 10,
-  company: "google",
+  price: 74,
+  company: "Urban Outfitters",
   info:
     "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
   inCart: false,
